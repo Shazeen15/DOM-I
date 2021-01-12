88f50676084553197d4a6ab7d4e2343d626edc6e
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-11 17:01:34 -0800

    digital timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,12 +1,23 @@
 const digit = document.querySelector(".digits");
 
-const secondTens = document.querySelector(".digits");
+const secondTens = document.querySelector(".secondTens");
+const secondOnes = document.querySelector(".secondOnes");
+const msHundreds = document.querySelector(".msHundreds");
+const msTens = document.querySelector(".msTens");
 
-const startingTime = 0;
-let time = startingTime * 60;
+// const startingTime = 0;
+// let time = startingTime * 60;
 
-function timer() {
-  let seconds = time % 60;
-  digit.innerHTML = `${seconds}`;
-  time++;
-}
+// function timer() {
+//   let seconds = time % 60;
+//   seconds = seconds < 10 ? "0" + seconds : seconds;
+//   secondTens.innerHTML = `${seconds}`;
+//   time++;
+
+//   if (time > 10) {
+//     time = 0;
+//     secondTens.classList.add("redDigit");
+//   }
+// }
+
+// setInterval(timer, 1000);
